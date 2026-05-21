66152f286b6382e944b9de029e6826a9d90a8247
Seril Evanjaline <serilevanjaline122006@gmail.com>
2026-05-21 16:31:08 +0530

    feat : connected PostgreSQL, URLs now persist, click tracking added

--- index.js
+++ index.js
@@ -1,37 +1,75 @@
 const express = require("express");
 const port = 8080;
 const app = express();
+const { Pool } = require("pg");
 
-//because request is not visible inn node.js , we need expreess
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "snip",
+    password: "postgres",
+    port: 5432,
+});
+
+
 app.use(express.json());
 
 const urls = new Map();
 
-//return the urls
 app.get("/urls", (req, res) => {
-    //since map cannot be deserialize to json , so as objects
     res.json(Object.fromEntries(urls));
 });
 
-app.post("/urls", (req, res) => {
 
-    const q = req.body.url;
-    const code = Math.random().toString(36).slice(2, 6);
-    urls.set(code, q);
-    res.status(200).json({ message: "Successfully Added a URL" });
+app.post("/urls", async (req, res) => {
+    try {
+        const { url } = req.body;
+        const code = Math.random().toString(36).slice(2, 6);
+
+        await pool.query(
+            "INSERT INTO urls (short_code, original_url) VALUES ($1, $2)",
+            [code, url]
+        )
+        res.status(200).json({ message: "Successfully Added a URL" });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
 
 });
 
-app.get("/:code", (req, res) => {
 
-    const q = req.params.code;
-    if (urls.get(q) !== undefined) {
-        res.json(urls.get(q));
-    }
-    else {
-        res.status(400).json("There is no URL ");
-    }
+app.get("/test-db", async (req, res) => {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows[0]);
+})
 
+
+app.get("/:code", async (req, res) => {
+    try {
+        const { code } = req.params;
+        const result = await pool.query(
+            "SELECT original_url FROM urls WHERE short_code = $1",
+            [code]
+        );
+
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: "URL not found" });
+        }
+
+        await pool.query(
+            "UPDATE urls SET click_count = click_count + 1 WHERE short_code = $1",
+            [code]
+        );
+
+        res.redirect(result.rows[0].original_url);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
 });
 
 
