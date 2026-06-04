30644d6361a5ee2144e5f662ec7fbe3aca0c3220
Seril Evanjaline <serilevanjaline122006@gmail.com>
2026-06-04 16:02:12 +0530

    fix: add TLS and password for Upstash Redis

--- redis.js
+++ redis.js
@@ -1,8 +1,10 @@
 const Redis = require("ioredis");
 
 const redis = new Redis({
-    host: process.env.REDIS_HOST ,
+    host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+    tls: {}
 });
 
 redis.on("connect", () => console.log("Redis connected"));
